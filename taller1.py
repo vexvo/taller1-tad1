6cbe61d3ed0c182823a58b336711b823dedae1f1
vexvo <santiago.uribeg@autonoma.edu.co>
2022-09-07 15:39:53 -0500

    Punto b finished (no hacer c)

--- taller1.py
+++ taller1.py
@@ -5,5 +5,8 @@ class Supermarket:
         self.cleaning = []
         self.grains = []
 
-
-
+    def add_product(self, category, product_name, product_amt: int):
+        if (category == "dairy"): self.dairy.append((product_name, product_amt))
+        if (category == "cleaning"): self.cleaning.append((product_name, product_amt))
+        if (category == "grains"): self.grains.append((product_name, product_amt))
+        else: print("Category entered does not exist")
